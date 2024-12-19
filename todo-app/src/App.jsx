1beefc2cf50fc579418e326115aabe39a4d65c8c
PAVANKUMAR-prg <pavankumar.koppa2004@gmail.com>
2024-12-19 19:34:32 +0530

    Final commit

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -1,3 +1,4 @@
+//app.jsx file
 import React from 'react';
 import './index.css';
 import TodoList from './components/ToDoList';
--- todo-app/src/components/TodoList.jsx
+++ todo-app/src/components/TodoList.jsx
@@ -1,3 +1,4 @@
+//todolist.jsx file
 import React, { useState, useEffect } from 'react';
 
 const TodoList = () => {
